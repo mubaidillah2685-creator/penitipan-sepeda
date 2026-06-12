0b79246c71e2c5fd28c248878254a8f26b5e7b71
mubaidillah2685-creator <m.ubaidillah2685@gmail.com>
2026-06-12 09:41:12 +0700

    test deploy

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2/promise';
 import { env } from '$env/dynamic/private';
 
 const db = mysql.createPool({
-	host: env.MYSQL_HOST,
-	port: Number(env.MYSQL_PORT),
-	user: env.MYSQL_USER,
-	password: env.MYSQL_PASSWORD,
-	database: env.MYSQL_DATABASE,
+	host: env.MYSQLHOST,
+	port: Number(env.MYSQLPORT),
+	user: env.MYSQLUSER,
+	password: env.MYSQLPASSWORD,
+	database: env.MYSQLDATABASE,
 	waitForConnections: true,
 	connectionLimit: 10
 });
