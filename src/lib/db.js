aadce188b3a20422c2f08ecb9b4bd00d076869d1
mubaidillah2685-creator <m.ubaidillah2685@gmail.com>
2026-06-12 09:45:50 +0700

    test

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,13 +2,19 @@ import mysql from 'mysql2/promise';
 import { env } from '$env/dynamic/private';
 
 const db = mysql.createPool({
-	host: env.MYSQLHOST,
-	port: Number(env.MYSQLPORT),
-	user: env.MYSQLUSER,
-	password: env.MYSQLPASSWORD,
-	database: env.MYSQLDATABASE,
-	waitForConnections: true,
-	connectionLimit: 10
+	host: env.MYSQL_HOST,
+	port: Number(env.MYSQL_PORT),
+	user: env.MYSQL_USER,
+	password: env.MYSQL_PASSWORD,
+	database: env.MYSQL_DATABASE
 });
+
+try {
+	const conn = await db.getConnection();
+	console.log('MYSQL CONNECTED');
+	conn.release();
+} catch (err) {
+	console.error('MYSQL ERROR:', err);
+}
 
 export default db;
